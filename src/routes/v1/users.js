fad81b559f520844e22fb46781ede669d407c98d
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-02 11:44:15 +0800

    Add GET route for getting all users in V1 of controllers

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -3,6 +3,8 @@ const { ControllersV1 } = require('../../controllers');
 
 const router = express.Router();
 
+router.get('/', ControllersV1.UsersController.getAllUsers);
+
 router.post('/register', ControllersV1.UsersController.addUser);
 
 module.exports = router;
