d78c56a2d20e98f6826f3686ecdca70df69a5600
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-02 09:43:10 +0800

    Add Auth routes to express router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const UserRoutes = require("./users");
+const AuthRoutes = require('./auth');
 
 const router = express.Router();
 
 router.use("/users", UserRoutes);
+router.use("/auth", AuthRoutes);
 
 module.exports = router;
