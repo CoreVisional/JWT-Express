65c3943fd78915ecf9621f024936f45e5f06840b
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-02 11:51:10 +0800

    Update get.js in users controller to exclude passwordSalt and passwordHash when getting back all users

--- src/controllers/v1/users/get.js
+++ src/controllers/v1/users/get.js
@@ -4,7 +4,7 @@ const getAllUsers = async (req, res) => {
 
     try {
 
-        const users = await User.find({});
+        const users = await User.find({}).select("-passwordSalt -passwordHash");
 
         res.status(200).json({
             status: 'success',
