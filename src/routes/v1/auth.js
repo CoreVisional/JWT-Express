5798b8d0cbd75556aec35f37cf97f94a2de740d2
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-02 11:21:24 +0800

    Update auth.js to add API endpoint to change user password

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const jwtValidation = require('../../middlewares/jwtMiddleware');
 const { ControllersV1 } = require("../../controllers");
 
 const router = express.Router();
 
-router.post("/login", ControllersV1.AuthController.login);
+router.post('/login', ControllersV1.AuthController.login);
+router.put(
+    '/change-password',
+    jwtValidation.required,
+    ControllersV1.AuthController.changePassword
+);
 
 module.exports = router;
