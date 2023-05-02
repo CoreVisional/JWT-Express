23c5b55f9eebf434d466f406bc704870098b5296
Alex Chiew <alex.chiewyz@gmail.com>
2023-05-02 11:41:36 +0800

    Add getAllUsers function to retrieve all users from the database

---
+++ src/controllers/v1/users/get.js
@@ -0,0 +1,26 @@
+const User = require('../../../models/user');
+
+const getAllUsers = async (req, res) => {
+
+    try {
+
+        const users = await User.find({});
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                users
+            }
+        })
+
+    } catch(error) {
+
+        res.status(500).json({
+            error: error.message
+        })
+    }
+};
+
+module.exports = {
+    getAllUsers,
+};
